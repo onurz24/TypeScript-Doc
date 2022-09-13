005aeea1bd2fc867c88008b81bd317653a3a811d
onurz24 <87843515+onurz24@users.noreply.github.com>
2022-09-13 15:47:58 +0200

    Update Static.js

--- Static.js
+++ Static.js
@@ -5,13 +5,5 @@ var Human = /** @class */ (function () {
     return Human;
 }());
 var JohnDoe = new Human();
-/*
-console.log(JohnDoe.atmen)
 
-Will Throw an Error because
- breathe no longer refers to himself/the
- new JohnDoe as Instance of the Class
- BUT to the Class itself / Class Mensch
-*/
-/* So to Console.log() the Class Variable we have to Call it like this */
 console.log(Human.breathe);
